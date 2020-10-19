d4d48268c33380a2da30f3cdf6f8f0fb30fcddd5
denoide <denoide1@gmail.com>
2020-10-19 13:12:44 +0200

    tu ma boddana

--- TestGitHub.java
+++ TestGitHub.java
@@ -4,7 +4,7 @@ public class TestGitHub{
     public static void main(String[] args){
 
         System.out.println("Sono nel main di Test GitHub Gianmarco");
-        System.out.println("PULLO FRITTO FAFAFAFAFA");
+        System.out.println("PULLO FRITTO PULLARE");
 
     }
 
