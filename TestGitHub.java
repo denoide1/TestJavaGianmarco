cd9f315b0312f8a82b042d78f529f92735dd8928
denoide <denoide1@gmail.com>
2020-10-19 22:24:01 +0200

    prova

--- TestGitHub.java
+++ TestGitHub.java
@@ -1,10 +1,10 @@
 public class TestGitHub{
 
-    // Mi faccio una pera
+    // Mi faccio una pera :v 
     public static void main(String[] args){
 
-        System.out.println("Sono nel main di Test GitHub Gianmarco");
-        System.out.println("PULLO FRITTO PULLARE");
+        int c = 4;
+        //AHAHAHA
 
     }
 
